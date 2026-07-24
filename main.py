30587c3e19adb6d6ec5cb1e468ba37892cfd85be
bdwerth <101779670+SuperiorSeaPickle@users.noreply.github.com>
2026-07-24 18:46:28 -0400

    Continuation with Annotation and Viewer

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ import scanpy as sc
 import anndata as ad
 from scipy.sparse import csc_matrix
 from sklearn.cluster import KMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
+import spatialdata as sd
+import spatialdata_io as sd_io
+from spatialdata.transformations import Identity
+from spatialdata.models import PointsModel, Image2DModel,ShapesModel,TableModel
+import dask.dataframe as dd
+import dask.array as da
+import tifffile
+import geopandas as gpd
 
 DATA_DIR = Path(r"D:\SPSC-RNA-Seq\WTA_Preview_FFPE_Breast_Cancer_outs")
 
@@ -401,7 +411,7 @@ def format_h5(tc_associations):
 
     print("Finished writing sparse CSC matrix.")
 
-def create_UMAP(cell_matrix_h5, from_file = False):
+def create_UMAP(cell_matrix_h5, from_file = False, view_plots=True):
     if not from_file:   
         print("loading matrix...")
         with h5py.File(cell_matrix_h5, 'r') as f:
@@ -421,10 +431,12 @@ def create_UMAP(cell_matrix_h5, from_file = False):
             adata.var_names = gene_names
             adata.obs_names = cell_names
 
-        # Keep genes expressed in at least 3 cells and cells with at least 200 genes
+        # Keep genes expressed in at least 3 cells and cells with at least 200 genes\
         print("cleaning data...")
         sc.pp.filter_cells(adata, min_genes=200)
         sc.pp.filter_genes(adata, min_cells=3)
+
+        adata.layers["counts"] = adata.X.copy()
 
         print("normalizing data...")
         sc.pp.normalize_total(adata, target_sum=1e4)
@@ -432,23 +444,44 @@ def create_UMAP(cell_matrix_h5, from_file = False):
 
         print("finding highly variable genes...")
         sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-        adata = adata[:, adata.var.highly_variable].copy()
+        adata_hvg = adata[:, adata.var.highly_variable].copy()
+
         print("rescaling...")
-        sc.pp.scale(adata, max_value=10, zero_center= False)
+        sc.pp.scale(adata_hvg, max_value=10, zero_center= False)
         print("running PCA...")
-        sc.tl.pca(adata,n_comps=50, random_state=0)
+        sc.tl.pca(adata_hvg,n_comps=50, random_state=0)
 
         print("generating UMAP...")
-        sc.pp.neighbors(adata,n_neighbors=15,n_pcs=50)
-        sc.tl.umap(adata)
+        sc.pp.neighbors(adata_hvg,n_neighbors=15,n_pcs=50)
+        sc.tl.umap(adata_hvg)
         
         print("creating KNN groups...")
-        sc.tl.leiden(adata, resolution=0.75, flavor="igraph", n_iterations=2)
+        sc.tl.leiden(adata_hvg, resolution=0.75, flavor="igraph", n_iterations=2)
+
+        # Embeddings
+        adata.obsm["X_pca"] = adata_hvg.obsm["X_pca"]
+        adata.obsm["X_umap"] = adata_hvg.obsm["X_umap"]
+
+        # Neighbor graph (optional, but useful)
+        adata.obsp["connectivities"] = adata_hvg.obsp["connectivities"]
+        adata.obsp["distances"] = adata_hvg.obsp["distances"]
+
+        # Metadata
+        adata.obs["leiden"] = adata_hvg.obs["leiden"]
+
+        # Copy relevant unstructured data
+        for key in ["pca", "neighbors", "umap", "leiden"]:
+            adata.uns[key] = adata_hvg.uns[key]
+            
         print("saving progress...")
-        adata.write_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
+        adata.write_h5ad(
+            str(DATA_DIR / "tmp" / "adata_tmp.h5ad"),
+            compression="lzf"
+        )
     else:
-        adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
-        sc.pl.umap(adata, color= "leiden")
+        adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"), backed="r+")
+        if view_plots:
+            sc.pl.umap(adata, color= "leiden")
 
     while True:
         print("type how many clusters you see:")
@@ -457,113 +490,236 @@ def create_UMAP(cell_matrix_h5, from_file = False):
             break
         except:
             print("invalid integer input")
+    if view_plots:
+        kmeans = KMeans(n_clusters=nclust, random_state=0).fit(adata.obsm['X_pca'])
+        adata.obs['kmeans_5'] = kmeans.labels_.astype(str)
+        sc.pl.umap(adata,color='kmeans_5')
 
-    kmeans = KMeans(n_clusters=nclust, random_state=0).fit(adata.obsm['X_pca'])
-    adata.obs['kmeans_5'] = kmeans.labels_.astype(str)
-    sc.pl.umap(adata,color='kmeans_5')
-
-def diff_analysis():
+def diff_analysis(view_plots = True):
     print("loading matrix...")
     adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
-    print(adata)
+    print("ranking gene groups...")
+    sc.tl.rank_genes_groups(adata, groupby="leiden", method="wilcoxon",reference="rest", use_raw=False )
 
-    sc.tl.rank_genes_groups(adata, groupby="leiden", method="wilcoxon", key_added="rank_markers", )
-    sc.pl.rank_genes_groups_heatmap(adata,n_genes=3, key="rank_markers")
-    sc.pl.rank_genes_groups_stacked_violin(adata, n_genes=3, groupby='leiden')
+    if view_plots:
+        sc.pl.rank_genes_groups(adata, n_genes=20,sharey=False)
+        sc.pl.rank_genes_groups_heatmap(adata, n_genes=10, groupby="leiden", show_gene_labels=True,cmap="viridis")
+        sc.pl.rank_genes_groups_dotplot(adata, n_genes=5, standard_scale="var")
 
     print("saving progress...")
-    adata.write_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
+    adata.write_h5ad(
+        str(DATA_DIR / "tmp" / "adata_tmp.h5ad"),
+        compression="lzf"
+    )
     
-    marker_df = pd.DataFrame(adata.uns["rank_markers"]["names"]).head(10)
+    marker_df = pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(20)
     print(marker_df)
 
-def annotate_cells(auto=False):
-    import decoupler as dc
+def validate_user_input(comm_dict, gene_dict):
+    
+    while True:
+        user_input = input()
+        if user_input in comm_dict:
+            if user_input == "[help]":
+                print(comm_dict)
+            else:    
+                break
+        else:
+            if user_input in gene_dict:
+                break
+            else:
+                print("invalid command or gene. Type [help] to see a list of valid commands.")
+    return user_input
+
+
+def annotate_cells(auto=True, view_figures= True):
+    import celltypist
     
 
     print("loading matrix...")
-    adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
-
-    diff_output = pd.DataFrame(adata.uns["rank_markers"]["names"]).head(10)
+    adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"), backed="r+")
+    print(adata)
+    diff_output = pd.DataFrame(adata.uns['rank_genes_groups']["names"]).head(10)
     cell_annots = {}
+    tmp_cpd = {
+        "unassinged": "#727171",  # Light Orange / Orange
+        'selected'  : "#56B4E9",  # Sky Blue  # Reddish Purple
+        'assigned'  : "#000000"   # Black
+    }
+
     if auto:
-        # markers = dc.op.resource("PanglaoDB", organism="human")
-        # markers = markers[
-        # markers["human"].astype(bool)
-        # & markers["canonical_marker"].astype(bool)
-        # & (markers["human_sensitivity"].astype(float) > 0.5)
-        # ]
-
-        # markers = markers.rename(
-        #     columns={
-        #         "cell_type": "source",
-        #         "genesymbol": "target"
-        #     }
-        # )[["source", "target"]]
-        # dc.mt.ora(
-        # data=adata,
-        # net=markers.drop_duplicates(subset=['source', 'target']),
-        # tmin=3,
-        # )
-
-        # # 1. Extract the scores from your AnnData object (replace 'ora_estimate' with your exact key)
-        acts = dc.pp.get_obsm(
+        celltypist.models.download_if_required()
+        celltypist.models.models_description()
+        predictions = celltypist.annotate(
             adata,
-            key="score_ora"
+            model="Cells_Adult_Breast.pkl",
+            majority_voting=True
         )
-
-        # print("saving progress...")
-        # adata.write_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
-
-        # 2. Use Scanpy's built-in heatmap function
-        sc.pl.heatmap(
-            acts,
-            var_names=acts.var_names,
-            groupby="leiden",
-            cmap="viridis"
-        )
-
-        scores = pd.DataFrame(
-            acts.X,
-            columns=acts.var_names
-        )
-
-        scores["leiden"] = acts.obs["leiden"].values
-
-        cluster_scores = (
-            scores
-            .groupby("leiden")
-            .mean()
-        )
-
-        sc.pl.matrixplot(
-            acts,
-            var_names=acts.var_names,
-            groupby="leiden",
-            standard_scale="var",
-            cmap="viridis"
-        )
-
-        annotation = cluster_scores.idxmax(axis=1)
-
-        print(annotation)
-    else:
-        print("Manual Mode: Name the given cell based on its marker genes listed below. Press [enter] to move on to the next group")
-
-        for i in range(diff_output.shape[1]):
-            print(diff_output.iloc[:,i])
-            print("annotate cell as: ",end="")
-            cell_annots[i] = input()
-        
-        adata.obs['cell_type'] = adata.obs['leiden'].map(cell_annots).astype('category')
-        
+        adata = predictions.to_adata()
         print("saving progress...")
-        adata.write_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"))
+        adata.write_h5ad(
+            str(DATA_DIR / "tmp" / "adata_tmp.h5ad"),
+            compression="lzf"
+        )
+        sc.pl.umap(
+            adata,
+            color="majority_voting",
+            size=2
+        )
+        ct_table = pd.crosstab(
+            adata.obs['leiden'], 
+            adata.obs['majority_voting'],
+            normalize="index"
+        )*100
 
-        print(adata)
+        # Display raw cell counts
+        print(ct_table.round(1))
 
-    
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(
+            ct_table, 
+            annot=True,        # Show percentages inside cells
+            fmt=".1f",         # 1 decimal place
+            cmap="Blues", 
+            cbar_kws={'label': 'Percentage of Cluster (%)'}
+        )
+        plt.title("Leiden Clusters vs. CellTypist Annotations")
+        plt.xlabel("CellTypist Majority Voting")
+        plt.ylabel("Leiden Cluster")
+        plt.tight_layout()
+        plt.show()
+    else:
+        commands = {
+            "[ec]annotation_name": "ends the creation of current cluster mask",
+            "[end]" : "complete cluster annotation and close",
+            "[set_thresh]": "change the default threshold (between 0 and 10)",
+            "[rcc]": "restart current cluster",
+            "[RAC]": "restart all clusters",
+            "[help]": "print instructions and command list"
+        }
 
+        valid_genes = pd.unique((pd.DataFrame(adata.uns['rank_genes_groups']["names"]).head(50)).values.ravel()).tolist()
+
+        print("Manual Mode: Use the figures that will be shown to create Expression filters.\n" \
+        "enter the genes by typing the name, then pressing enter to add another gene. type [ec] to finish the filter. for example:\n" \
+        "KIT \n" \
+        "CTSG \n" \
+        "MS4A2\n" \
+        "CPA3\n" \
+        "IL1RL1\n" \
+        "[ec]cell type 1\n" \
+        "an updating viewer will show you what's been selected\n" \
+        "Press [Enter] to continue...")
+
+        com = None
+        tmp_markers = []
+        markers = pd.DataFrame()
+        thresh = 2.0
+        cname = None
+
+        coords = adata.obsm["X_umap"]
+        adata.obs["annotation"] = "U"
+        fig, ax = plt.subplots()
+        scatter = ax.scatter(
+            coords[:, 0],
+            coords[:, 1],
+            c=tmp_cpd["unassinged"],
+            s=0.5
+        )
+        plt.show(block=False)
+
+        while True:
+            com = validate_user_input(commands, valid_genes)
+            if com.startswith("[ec]"):
+                cname = com[4:]
+                markers[cname] = tmp_markers
+                tmp_markers.clear()
+
+
+            elif com == "[end]":
+                break
+            elif com == "[set_thresh]":
+                while True:
+                    try:
+                        thresh = float(input())
+                        if not (0 <= thresh <= 10):
+                            raise ValueError(f"threshold must be between 0 and 1. Got: {thresh}")
+                    except:
+                        print("invalid input: must be float in interval [0,10]")
+            elif com == "[rcc]":
+                tmp_markers.clear()
+            elif com == "[RAC]":
+                markers = pd.DataFrame()
+            else:
+                tmp_markers.append(com)
+
+            expr = adata[:, tmp_markers].X
+            mask = np.ones(adata.n_obs, dtype=bool)
+            for i in range(len(tmp_markers)):
+                gene_expr = expr[:, i]
+
+                if hasattr(gene_expr, "toarray"):
+                    gene_expr = gene_expr.toarray().ravel()
+
+                mask &= gene_expr > thresh
+            adata.obs.loc[mask, "annotation"] = cname
+            groups = adata.obs["annotation"]
+            colors = np.full(
+                adata.n_obs,
+                tmp_cpd["assigned"],
+                dtype=object
+            )
+            colors[groups == "U"] = tmp_cpd["unassinged"]
+            colors[groups == cname] = tmp_cpd["selected"]
+            scatter.set_facecolors(colors)   # categorical RGB colors
+            fig.canvas.draw_idle()
+            plt.pause(0.01)
+        
+
+
+
+def view_spatial(adata):
+
+    # ddf = dd.read_parquet(str(DATA_DIR / "tmp" / "trns_with_cellID_regroup.parquet"),
+    # columns=[
+    #     "x_location",
+    #     "y_location",
+    #     "feature_name",
+    # ])
+    # points_element = PointsModel.parse(
+    #     ddf,
+    #     coordinates={'x': "x_location", 'y': "y_location"},
+    #     feature_key= "feature_name"
+    # )
+
+    ome_dask = da.from_zarr(tifffile.imread(str(DATA_DIR/"morphology.ome", aszarr=True)))
+    image_element = Image2DModel.parse(
+        data=ome_dask,
+        dims=('c','y','x'),
+        chunks=(1,1024,1024),
+        transformations={"global": Identity()}
+    )
+
+    gdf = gpd.read_parquet(str(DATA_DIR/"cell_boundaries.parquet"))
+    shape_trnsfr = {"global": Identity()}
+    shapes_element = ShapesModel.parse(gdf, transformations=shape_trnsfr)
+
+    adata.obs["region"] = "cell_boundries"
+    adata.obs["instance_id"] = adata.obs_names.astype(int)
+    table_element = TableModel.parse(
+        adata,
+        region="cell_boundries",
+        region_key="region",
+        instance_key="instance_id"
+    )
+    sdata = sd.SpatialData(
+    images={"tissue_image": image_element},
+    #points={"transcripts": points_element},
+    shapes={"cell_boundaries": shapes_element},
+    tables={"expression_table": table_element}
+)
+    interactive = interactive(sdata)
+    interactive.run()
 
 
 def total_count_scatter():
@@ -595,5 +751,9 @@ if __name__ == "__main__":
 
     #assign_gene_to_cell(DATA_DIR / "transcripts.parquet", DATA_DIR / "cell_boundaries.parquet")
     #format_h5(r"D:\SPSC-RNA-Seq\WTA_Preview_FFPE_Breast_Cancer_outs\tmp\trns_with_cellID.parquet")
-    create_UMAP(r"D:\SPSC-RNA-Seq\WTA_Preview_FFPE_Breast_Cancer_outs\tmp\cell_matrix.h5")
-    diff_analysis()
+    #create_UMAP(r"D:\SPSC-RNA-Seq\WTA_Preview_FFPE_Breast_Cancer_outs\tmp\cell_matrix.h5",view_plots=False)
+    #diff_analysis(view_plots=False)
+    #annotate_cells(auto=False)
+    print("loading matrix...")
+    adata = sc.read_h5ad(str(DATA_DIR / "tmp" / "adata_tmp.h5ad"), backed="r+")
+    view_spatial(adata)
